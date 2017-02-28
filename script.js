c24f83f768e9ecd04bbe57f2b25f16767cb250b4
Clifton1000 <grannyshane100@gmail.com>
2017-02-28 11:13:39 -0700

    chapter 4

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
  /*
  Project: Javascript
  Author: Shane Byrd
- Date: 2/20/17
+ Date: 2/27/17
  */
- 
-//chapter 2
 
 //set a global variable and initialize it to a string value of 4000
-var initialBalance = "4000";
-      //create a resetForm function for users to refresh page and return to value of 1
-function resetForm() {
-    document.getElementById("micros").value = 1;
 
-	createEventListeners();
-	
-}
+
+function resetForm() {
+    //create a resetForm function for users to refresh page and return to value of 1 for # of micros.
+    document.getElementById("micros").value = 1;
+    createEventListeners();
+  }
+    function buttonClickHandler() {//function that handles when the button is clicked
+        document.getElementById("number_error_display").innerHTML = "";
+        try {
+            if (document.getElementById("micros").value <= 0) {
+                throw "Please enter a number greater than 0";
+            }
+        }
+        catch(errorMsg) { //block of code to handle errors
+          //display error message
+          document.getElementById("number_error_display").innerHTML = errorMsg;
+	      //remove output display
+		  document.getElementById("output").style.display = "none";
+        }
+    }
+document.getElementById("calcBtn"). addEventListener("click", buttonClickHandler, false);
+
 
 function createEventListeners() {
-  //when someone clicks 'submit' the event handler click does its work
-    document.getElementById("calcBtn").addEventListener("click", calcMicros, false);
+  //when someone clicks 'submit' the event listener click does its work
+document.getElementById("calcBtn").addEventListener("click", calcMicros, false);
 	//For clicking on the other button
-	document.getElementById("tradeStyleBtn").addEventListener("click", tradeStyle, false);
+document.getElementById("tradeStyleBtn").addEventListener("click", tradeStyle, false);
 }
 
 function tradeStyle() {
-	
+
 	//console.log("Clicked");
 	var freeTime = document.getElementById("style").value;
-	// The code should make a decision based on the freeTime value
-	switch (freeTime) {
+	// The code makes a decision based on the freeTime value
+	switch (freeTime) {//Displays a string value depending on user input 1, 2, 3, 4.
 		case "1":
-			//JA: Display your string value on the page instead of returning a value
 			document.getElementById("traderPosition").innerHTML = "Position Trader";
 			//return "Position Trader";
 			break;
@@ -52,7 +64,7 @@ function tradeStyle() {
 		 	break;
 	}
 }
-
+var initialBalance = "4000";
 function calcMicros() {
   //convert the number from the form, which is read as a character
   // and converts it to numberical format.
@@ -60,12 +72,7 @@ function calcMicros() {
     var result = (initialNumber * initialBalance);
 
 //display the output
-    document.getElementById("output").innerHTML = "$" + result + " recommended account balance.";
-	
-	
+document.getElementById("output").innerHTML = "$" + result + " recommended account balance.";
 }
 //resets form when page is relaoded
 window.addEventListener("load", resetForm, false);
-
-
-
